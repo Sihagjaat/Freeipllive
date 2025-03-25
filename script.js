b4bc139f6f6ec6c4883f220180f74052f52a8f1f
Sihagjaat <amitsihag504@gmail.com>
2025-03-25 09:23:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function joinWhatsApp() {
 // HLS.js to load the M3U8 stream
 document.addEventListener("DOMContentLoaded", function () {
   var video = document.getElementById('live-stream');
-  var videoSrc = "https://v18tataplaysyndication.akamaized.net/bpk-tv/Sports18_1_HD_voot_MOB/output03/hdntl=exp=1742807949~acl=/*~data=hdntl~hmac=82503849249783b714bd6b5996167a88e48a528916c1ac20b7dd8bae23538118/Sports18_1_HD_voot_MOB-audio_108038_eng=108000-video=2297600.m3u8";
+  var videoSrc = "https://mtsft.ruscfd.lat/720p.m3u8";
 
   if (Hls.isSupported()) {
     var hls = new Hls();
